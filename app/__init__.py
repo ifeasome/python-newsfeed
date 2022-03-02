44de8bd55ea708db29640d8eb31fe40cf9f9f697
Somelina Obiechina <ifeasome@gmail.com>
2022-03-01 20:18:45 -0600

    created dashboard view routes

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from app.routes import home
+from app.routes import home, dashboard
 
 def create_app(test_config=None):
   # set up app config
@@ -16,4 +16,5 @@ def create_app(test_config=None):
 
 # register routes
   app.register_blueprint(home)
+  app.register_blueprint(dashboard)
   return app
